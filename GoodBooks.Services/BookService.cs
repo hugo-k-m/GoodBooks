30bf928b395cf33ee10649d3431ea3ad5494db5c
Hugo <57953645+hugo-k-m@users.noreply.github.com>
2020-09-21 18:49:10 +0200

    Scaffolded the book service.

---
+++ GoodBooks.Services/BookService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using GoodBooks.Data;
+using GoodBooks.Data.Models;
+
+namespace GoodBooks.Services
+{
+    public class BookService : IBookService
+    {
+        private readonly GoodBooksDbContext _db;
+
+        public BookService(GoodBooksDbContext db)
+        {
+            _db = db;
+        }
+
+        public void AddBook(Book book)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteBook(int bookId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Book> GetAllBooks()
+        {
+            throw new NotImplementedException();
+        }
+
+        public BookService GetBook(int bookId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
