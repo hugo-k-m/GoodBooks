7ad5a008144de066f4724add461df4d3eff3310b
Hugo <57953645+hugo-k-m@users.noreply.github.com>
2020-12-14 12:29:47 +0200

    Fixed a bug by moving the .env file into the Vue project. HTTPS redirection from the API start up configuration.

--- GoodBooks.Web/Startup.cs
+++ GoodBooks.Web/Startup.cs
@@ -41,7 +41,7 @@ namespace GoodBooks.Web
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
+            // app.UseHttpsRedirection();
 
             app.UseRouting();
 
